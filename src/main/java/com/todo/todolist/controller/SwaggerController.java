b0217a173acbfe8d870a81429b8e7e41ba40de38
Hwi <124661622+Imhwitae@users.noreply.github.com>
2023-08-03 15:14:18 +0900

    깃 이그노어 설정

--- src/main/java/com/todo/todolist/controller/SwaggerController.java
+++ src/main/java/com/todo/todolist/controller/SwaggerController.java
@@ -1,5 +1,8 @@
 package com.todo.todolist.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SwaggerController {
-    
 }
