b6c1c795fbbc9715c687a9de798ce1d0abb7e010
Hwi <124661622+Imhwitae@users.noreply.github.com>
2023-08-04 13:40:53 +0900

    swagger-ui 작성

--- src/main/java/com/todo/todolist/controller/SwaggerController.java
+++
@@ -1,8 +0,0 @@
-package com.todo.todolist.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class SwaggerController {
-}
