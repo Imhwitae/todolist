a6f8be1c2483a5f810eac9ebba8278ad88efa106
Hwi <124661622+Imhwitae@users.noreply.github.com>
2023-08-09 01:04:29 +0900

    구글 로그인 토큰 받아야함

--- src/main/java/com/todo/todolist/controller/MainController.java
+++ src/main/java/com/todo/todolist/controller/MainController.java
@@ -3,12 +3,14 @@ package com.todo.todolist.controller;
 import com.todo.todolist.config.auth.SessionMember;
 import com.todo.todolist.dto.request.MemLoginDto;
 import com.todo.todolist.entity.Members;
+import com.todo.todolist.service.LoginService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
     private final HttpSession httpSession;
+    private final LoginService loginService;
 
 //    @GetMapping
 //    public String signUpForm(@ModelAttribute("members") Members members,
@@ -25,19 +28,26 @@ public class MainController {
 //    }
 
     @GetMapping
-    public String mainPage(Model model) {
-        SessionMember member = (SessionMember) httpSession.getAttribute("member");
-
-        if (member != null) {
-            model.getAttribute(member.getName());
-        }
-
+    public String loginPage() {
         return "index";
     }
 
     @GetMapping("/kakao")
     public String kakaoLogin(){
         return "kakaologin";
+    }
+
+    @PostMapping
+    public String mainPage(Model model) {
+        SessionMember member = (SessionMember) httpSession.getAttribute("member");
+
+        model.getAttribute(member.getEmail());
+
+        if (member == null) {
+            return "redirect:/";
+        }
+
+        return "/todo_main";
     }
 }
 
--- src/main/java/com/todo/todolist/service/LoginService.java
+++ src/main/java/com/todo/todolist/service/LoginService.java
@@ -2,6 +2,8 @@ package com.todo.todolist.service;
 
 import java.util.List;
 import java.util.Optional;
+
+import com.todo.todolist.config.auth.SessionMember;
 import com.todo.todolist.entity.Members;
 import com.todo.todolist.repository.MembersRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +22,13 @@ public class LoginService {
 //        if (loginMember.contains(password)){
 //
 //        }
+//    }
+
+
+//    public String googleLogin(SessionMember member) {
+//        // String.valueOf(): 만약 값이 null이라면 그냥 문자열 null로 표기
+//        String memberEmail = String.valueOf(membersRepository.findByEmail(member.getEmail()));
+//
+//
 //    }
 }
